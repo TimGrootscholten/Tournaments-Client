454dd6125e3240b44c402794aa89d794ba1fe518
Tim Grootscholten <timgrootscholtentim@gmail.com>
2022-02-18 12:56:29 +0100

    test
    login registreer

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Home from "./Pages/Home";
+import Login from "./Pages/Login";
+import Registreren from "./Pages/Registreren";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/">
+            <Route index element={<Home />} />
+            <Route path="login" element={<Login />} />
+            <Route path="registreer" element={<Registreren />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
