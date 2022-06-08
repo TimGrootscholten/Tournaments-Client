7aa3d24447ca9c6a0bab763994e9f1de90474f9b
Davement <50558281+Davement@users.noreply.github.com>
2022-06-08 20:24:45 +0200

    Token service fix

--- src/services/token.service.ts
+++ src/services/token.service.ts
@@ -1,38 +1,32 @@
-import { openDB, DBSchema } from "idb";
+import { openDB, DBSchema, IDBPDatabase } from "idb";
 
 interface MyDB extends DBSchema {
-    Tournaments: {
-      key: string;
-      value: string;
-    };
-  }
+  Tournaments: {
+    key: string;
+    value: string;
+  };
+}
+
+async function getTournamentsDb(): Promise<IDBPDatabase<MyDB>> {
+  return await openDB<MyDB>("Tournaments", 1, {
+    upgrade(db) {
+      db.createObjectStore("Tournaments");
+    },
+  });
+}
 
 
-  export const clear = async () => {
-    const db = await openDB<MyDB>("Tournaments", 1, {
-        upgrade(db) {
-            db.createObjectStore("Tournaments");
-        },
-    });
+export const clear = async () => {
+  const db = await getTournamentsDb();
+  await db.clear("Tournaments");
+}
 
-    await db.clear("Tournaments");
-  }
+export const dbPut = async (value: string, key: string | IDBKeyRange | undefined) => {
+  const db = await getTournamentsDb();
+  await db.put("Tournaments", value, key);
+}
 
-  export const dbPut = async (value:string, key: string | IDBKeyRange | undefined ) => {
-    const db = await openDB<MyDB>("Tournaments", 1, {
-        upgrade(db) {
-            db.createObjectStore("Tournaments");
-        },
-    });
-
-    await db.put("Tournaments", value, key);
-  }
-
-  export const dbGet = async ( key: string | IDBKeyRange ) => {
-    const db = await openDB<MyDB>("Tournaments", 1, {
-        upgrade(db) {
-          db.createObjectStore("Tournaments");
-        },
-      });
-    return await db.get("Tournaments", key);
-  }
+export const dbGet = async (key: string | IDBKeyRange) => {
+  const db = await getTournamentsDb();
+  return await db.get("Tournaments", key);
+}
